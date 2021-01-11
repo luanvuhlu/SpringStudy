2bd6caa458cfd9969c0fa79d8ee2d28613082307
LinhChi1502 <linhchidkc@gmail.com>
2021-01-11 21:20:04 +0700

    delete result and add date in userexam

--- src/main/java/com/case6/quizchallengeweb/model/exam/UserExam.java
+++ src/main/java/com/case6/quizchallengeweb/model/exam/UserExam.java
@@ -3,8 +3,11 @@ package com.case6.quizchallengeweb.model.exam;
 import com.case6.quizchallengeweb.model.question.UserAnswer;
 import com.case6.quizchallengeweb.model.user.AppUser;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -14,7 +17,8 @@ public class UserExam {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private double result; 
+    @UpdateTimestamp
+    private Date date;
 
     @ManyToOne
     @JoinColumn(name = "appUser")
