23e85fd75c0e66cdbb79710797e6445006903841
toandh <toandh.thuyllinh@gmail.com>
2021-01-12 14:16:03 +0700

    them toan test controller

--- src/main/java/com/case6/quizchallengeweb/model/question/Answer.java
+++ src/main/java/com/case6/quizchallengeweb/model/question/Answer.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 public class Answer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String content;
