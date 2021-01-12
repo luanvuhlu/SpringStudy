00c101e2b367b175ebae832b7a3a3496839e2bef
toandh <toandh.thuyllinh@gmail.com>
2021-01-12 21:07:50 +0700

    add validation to category.done

--- src/main/java/com/case6/quizchallengeweb/controller/CategoryController.java
+++ src/main/java/com/case6/quizchallengeweb/controller/CategoryController.java
@@ -4,10 +4,15 @@ import com.case6.quizchallengeweb.service.question.category.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -25,13 +30,22 @@ public class CategoryController {
 
 
     @PostMapping
-    public ResponseEntity<Category> insertCategory(@RequestBody Category category){
+    public ResponseEntity<Category> insertCategory(@Valid @RequestBody Category category){
         categoryService.save(category);
         return new ResponseEntity<>(category, HttpStatus.ACCEPTED);
     }
 
-
-
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            ((HashMap) errors).put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
 }
